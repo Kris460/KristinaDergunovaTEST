b2965341535f8bc185c2417e01f9146c6605ccc3
Kristina <testingfox460@gmail.com>
2021-01-31 15:01:39 +0100

    First Homework Dergunova

---
+++ src/CourseJava/Online/Test1.java
@@ -0,0 +1,53 @@
+package CourseJava.Online;
+
+public class Test1 {
+
+    public static void main (String[] args) {
+
+
+        int a = 63;
+        int b = 48;
+        int c = 11;
+        double d = 15;
+        double e = a * (b + (c / d));
+        System.out.println(e);
+
+
+
+
+        int f = 12;
+        int g = 1;
+        int summary = f + g;
+
+        if (summary < 10) {
+            System.out.println("False");
+        }
+        if (summary > 20) {
+            System.out.println("False");
+        } else {
+            System.out.println("True");
+        }
+
+
+    int h = 0;
+       if(h >-1)
+
+    {
+        System.out.println("Положительное число");
+    }
+       if(h<0)
+
+    {
+        System.out.println("Отрицательное число");
+    }
+
+    String yourName = "Kris";
+    System.out.println("Hello," + yourName + "!");
+}
+
+}
+
+
+
+
+
